5aa022ec5cedf6b5c384824cbf876da368339239
kanisthaw <s7070011@kmitl.ac.th>
2014-11-24 11:49:51 +0700

    change ipady and ipadx

--- pianoforte.py
+++ pianoforte.py
@@ -5,7 +5,7 @@ bgcolor = "#6C7A89"
 root = Tk()
 root.config(bg=bgcolor)
 root.title("Pianoforte")
-root.geometry("302x500+150+150")
+root.geometry("500x500")
 
 
 #FUNTION FOR CHANGING IMAGE
@@ -26,7 +26,7 @@ mlabel = Label( root, image=img)
 mlabel.pack()
 #FRAME01
 frame = Frame(root, bg=bgcolor)
-frame.pack(fill=X)
+frame.pack(fill=X, pady=10)
 #MAJORCHORDNAME
 var = StringVar()
 label = Label( frame, textvariable=var, bg=bgcolor)
@@ -34,46 +34,44 @@ var.set("Major Chords")
 label.pack(side=LEFT)
 #MAJORCHORD
 cord_c = Button(frame, text="C", fg="red", command = printphoto_C)
-cord_c.pack(side=LEFT)
+cord_c.pack(side=LEFT, ipadx=4)
 
 cord_Db = Button(frame, text="Db", fg="red", command = printphoto_Db)
-cord_Db.pack(side=LEFT)
+cord_Db.pack(side=LEFT, ipadx=4)
 
 cord_D = Button(frame, text="D", fg="red", command = printphoto_C)
-cord_D.pack(side=LEFT)
+cord_D.pack(side=LEFT, ipadx=4)
 
 cord_Eb = Button(frame, text="Eb", fg="red", command = printphoto_C)
-cord_Eb.pack(side=LEFT)
+cord_Eb.pack(side=LEFT, ipadx=4)
 
 cord_E = Button(frame, text="E", fg="red", command = printphoto_C)
-cord_E.pack(side=LEFT)
+cord_E.pack(side=LEFT, ipadx=4)
 
 cord_F = Button(frame, text="F", fg="red", command = printphoto_C)
-cord_F.pack(side=LEFT)
+cord_F.pack(side=LEFT, ipadx=4)
 
 cord_FL = Button(frame, text="F#", fg="red", command = printphoto_C)
-cord_FL.pack(side=LEFT)
+cord_FL.pack(side=LEFT, ipadx=4)
 
 cord_Gb = Button(frame, text="Gb", fg="red", command = printphoto_C)
-cord_Gb.pack(side=LEFT)
+cord_Gb.pack(side=LEFT, ipadx=4)
 
 cord_G = Button(frame, text="G", fg="red", command = printphoto_C)
-cord_G.pack(side=LEFT)
+cord_G.pack(side=LEFT, ipadx=4)
 
 cord_Ab = Button(frame, text="Ab", fg="red", command = printphoto_C)
-cord_Ab.pack(side=LEFT)
+cord_Ab.pack(side=LEFT, ipadx=4)
 
 cord_A = Button(frame, text="A", fg="red", command = printphoto_C)
-cord_A.pack(side=LEFT)
+cord_A.pack(side=LEFT, ipadx=4)
 
 cord_Bb = Button(frame, text="Bb", fg="red", command = printphoto_C)
-cord_Bb.pack(side=LEFT)
+cord_Bb.pack(side=LEFT, ipadx=4)
 
 cord_B = Button(frame, text="B", fg="red", command = printphoto_C)
-cord_B.pack(side=LEFT)
+cord_B.pack(side=LEFT, ipadx=4)
 
-cord_ = Button(frame, text="Ab", fg="red", command = printphoto_C)
-cord_.pack(side=LEFT)
 
 #FRAME02
 frame = Frame(root, bg=bgcolor)
